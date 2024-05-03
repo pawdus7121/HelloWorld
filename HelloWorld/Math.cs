0088985c913c22e58ed8f8a862fcb25e837c335f
Hp <Hp@DESKTOP-J3ULP9I>
2024-05-03 09:30:22 +0200

    Add project files.

---
+++ HelloWorld/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Helloworld
+{
+    public class Math
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Helloworld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a = 20;
+            double b = 10;
+            Math math = new Math();
+            double Addresult = math.Add(a, b);
+
+            double substractResult = math.Substract(a, b);
+
+            Console.WriteLine(Addresult.ToString());
+
+            Console.WriteLine(substractResult.ToString());
+        }
+    }
+}
